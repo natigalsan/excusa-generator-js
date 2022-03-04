1e6722f6875aa51a90bc1f7c862a237a2cbf652c
natigalsan <natividadgalsan@gmail.com>
2022-03-04 23:00:54 +0100

    vainillajs-hello V-ok
    primera version que funciona

--- src/app.js
+++ src/app.js
@@ -1,11 +1,54 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+function onLoad() {
   //write your code here
+
+  
+  let  who  =  [ 'El perro' , 'Mi abuela' , 'Su tortuga' , 'Mi pájaro' ] ; 
+  let  action  =  [ 'comió' , 'meó' , 'aplastó' , 'rompió' ] ; 
+  let  what  =  [ 'mi tarea' ,  'las llaves' ,  'el carro' ] ; 
+  let  when  =  [ 'antes de la clase' , 'justo a tiempo' , 'cuando terminé','mientras estaba rezando' ] ;
+  
+  
+  var frase='';
+  var numerorandom;
+  // La frase tiene 4 partes: who + action + what + when. Hay que calcular 4 elementos aleatorios
+  for(let i=0;i<4;i++){
+   
+    //Por cada parte de la frase, elijo una palabra aleatoria
+    
+    switch (i) {
+      case 0:
+      numerorandom = Math.floor((Math.random()*who.length));  
+      frase += who[numerorandom];
+        break;
+      case 1:
+        numerorandom = Math.floor((Math.random()*action.length)); 
+        frase += " " + action[numerorandom];
+        break;
+      case 2:
+        numerorandom = Math.floor((Math.random()*what.length)); 
+        frase += " " + what[numerorandom];
+        break;
+      default:
+        numerorandom = Math.floor((Math.random()*when.length)); 
+        frase += " " + when[numerorandom];
+        break;
+    }
+  }
+  
+  
+  
   console.log("Hello Rigo from the console!");
+
+  return frase;
+
 };
+
+var varexcusa=onLoad();
+document.getElementById("excusa").innerHTML = varexcusa;
